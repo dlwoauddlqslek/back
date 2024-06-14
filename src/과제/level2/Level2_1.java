d86cf4c472704cf5613b04cb52ab7b7f393e5bd1
dlwoauddlqslek <ljm5857@gmail.com>
2024-06-14 18:20:25 +0900

    2

--- src/과제/level2/Level2_1.java
+++ src/과제/level2/Level2_1.java
@@ -6,6 +6,11 @@ public class Level2_1 {
   public static void main(String[] args) {
     Scanner scan=new Scanner(System.in);
     while(true){
+      System.out.print(" 음악 재생 중 종료버튼[x] : ");
+      char btn = scan.next().charAt(0);
+      if (btn=='x'){
+        System.out.println(">> 음악종료"); break;
+      }
 
     }
   }
